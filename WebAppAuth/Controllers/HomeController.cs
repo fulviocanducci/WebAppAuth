718ad27c05242c9f1e4f9f0c27c8a02aa99bcbe2
Fúlvio <fulviocanducci@hotmail.com>
2020-06-20 01:13:34 -0300

    Add project files.

---
+++ WebAppAuth/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace WebAppAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> Logger { get; }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            Logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated == false)
+            {
+                Claim[] claims = new Claim[5]
+                {
+                    new Claim("Full Name", "Fulvio Cezar Canducci Dias"),
+                    new Claim(ClaimTypes.Name , "fulviocanducci@hotmail.com"),
+                    new Claim(ClaimTypes.Email, "fulviocanducci@hotmail.com"),
+                    new Claim(ClaimTypes.Role, "admin"),
+                    new Claim(ClaimTypes.Role, "user"),
+                };
+
+                AuthenticationProperties properties = new AuthenticationProperties
+                {
+                    //AllowRefresh = <bool>,
+                    // Refreshing the authentication session should be allowed.
+
+                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                    // The time at which the authentication ticket expires. A 
+                    // value set here overrides the ExpireTimeSpan option of 
+                    // CookieAuthenticationOptions set with AddCookie.
+
+                    //IsPersistent = true,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+
+                    //IssuedUtc = <DateTimeOffset>,
+                    // The time at which the authentication ticket was issued.
+
+                    //RedirectUri = <string>
+                    // The full path or absolute URI to be used as an http 
+                    // redirect response value.
+                };                
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, properties);
+                Logger.LogInformation("Login", "Login of User ...");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Authorize()]
+        public IActionResult Secret()
+        {           
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            Logger.LogInformation("Logout", "Logout of User ...");
+            return RedirectToAction("Index");
+        }        
+    }
+}
